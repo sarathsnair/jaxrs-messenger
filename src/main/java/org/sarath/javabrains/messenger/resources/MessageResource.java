a48d4d6260b87d2562076ed839ad33989530284b
Sarath S Nair <sarathsnairxyz@gmail.com>
2016-05-29 20:18:10 +0530

    Adding POST, PUT, DELETE methods

--- src/main/java/org/sarath/javabrains/messenger/resources/MessageResource.java
+++ src/main/java/org/sarath/javabrains/messenger/resources/MessageResource.java
@@ -9,20 +9,39 @@ import org.sarath.javabrains.messenger.model.Message;
 import org.sarath.javabrains.messenger.service.MessageService;
 
 @Path("/messages")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class MessageResource {
 
 	MessageService messageService = new MessageService();
-	
+
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
 	public List<Message> getMessages() {
 		return messageService.getAllMessages();
 	}
-	
+
 	@GET
 	@Path("/{messageId}")
-	@Produces (MediaType.APPLICATION_JSON)
-	public Message getMessage(@PathParam("messageId") long  messageId){
+	public Message getMessage(@PathParam("messageId") long messageId) {
 		return messageService.getMessage(messageId);
 	}
+
+	@POST
+	public Message addMessage(Message message) {
+		return messageService.addMessage(message);
+	}
+
+	@PUT
+	@Path("/{messageId}")
+	public Message updateMessage(@PathParam("messageId") long id, Message message) {
+		message.setId(id);
+		return messageService.updateMessage(message);
+	}
+
+	@DELETE
+	@Path("/{messageId}")
+	public void deleteMessage(@PathParam("messageId") long id) {
+		messageService.removeMessage(id);
+	}
+
 }
