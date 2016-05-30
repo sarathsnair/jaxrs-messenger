33b6f4af77928630a6b85930e7aff0b7483baebd
Sarath S Nair <sarath.nair@asda.co.uk>
2016-05-30 16:15:31 +0530

    Implementation of getAllMessageForYear(year) and getAllMessagesPaginated(start, size) methods

--- src/main/java/org/sarath/javabrains/messenger/resources/MessageResource.java
+++ src/main/java/org/sarath/javabrains/messenger/resources/MessageResource.java
@@ -16,7 +16,14 @@ public class MessageResource {
 	MessageService messageService = new MessageService();
 
 	@GET
-	public List<Message> getMessages() {
+	public List<Message> getMessages(@QueryParam("year") int year, @QueryParam("start") int start,
+			@QueryParam("size") int size) {
+		if (year > 0) {
+			return messageService.getAllMessageForYear(year);
+		}
+		if (start >= 0 && size > 0) {
+			return messageService.getAllMessagesPaginated(start, size);
+		}
 		return messageService.getAllMessages();
 	}
 
