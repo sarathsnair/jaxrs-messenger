254b14e55cbe6f6553fbbd0ba501a7001a9aff7e
Sarath S Nair <sarath.nair@asda.co.uk>
2016-05-31 18:19:32 +0530

    DataNotFoundException
    Defenition for the DataNotFoundException

---
+++ src/main/java/org/sarath/javabrains/messenger/exception/DataNotFoundException.java
@@ -0,0 +1,13 @@
+package org.sarath.javabrains.messenger.exception;
+
+public class DataNotFoundException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5880997475010205948L;
+
+	public DataNotFoundException(String message) {
+		super(message);
+	}
+}
