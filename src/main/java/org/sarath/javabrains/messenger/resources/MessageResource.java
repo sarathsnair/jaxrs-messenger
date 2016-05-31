4cb48fc58205046d5a1f3b32f572aa62ce9f5711
Sarath S Nair <sarath.nair@asda.co.uk>
2016-05-31 19:45:38 +0530

    Added HATEOS in MessageResource
    This will append the appropriate links to the message response

--- src/main/java/org/sarath/javabrains/messenger/resources/MessageResource.java
+++ src/main/java/org/sarath/javabrains/messenger/resources/MessageResource.java
@@ -45,9 +45,30 @@ public class MessageResource {
 
 	@GET
 	@Path("/{messageId}")
-	public Message getMessage(@PathParam("messageId") long messageId) {
-		return messageService.getMessage(messageId);
+	public Message getMessage(@PathParam("messageId") long messageId, @Context UriInfo uriInfo) {
+		Message message = messageService.getMessage(messageId);
+		message.addLink(getUriForSelf(uriInfo, message), "self");
+		message.addLink(getUriForProfile(uriInfo, message), "profile");
+		message.addLink(getUriForComments(uriInfo, message), "comments");
+		return message;
 	}
+
+	private String getUriForComments(UriInfo uriInfo, Message message) {
+		URI uri = uriInfo.getBaseUriBuilder().path(MessageResource.class)
+				.path(MessageResource.class, "getCommentResource").path(CommentResource.class)
+				.resolveTemplate("messageId", message.getId()).build();
+		return uri.toString();
+	}
+
+	private String getUriForProfile(UriInfo uriInfo, Message message) {
+		URI uri = uriInfo.getBaseUriBuilder().path(ProfileResource.class).path(message.getAuthor()).build();
+		return uri.toString();
+	}
+
+	private String getUriForSelf(UriInfo uriInfo, Message message) {
+		URI uri = uriInfo.getBaseUriBuilder().path(MessageResource.class).path(Long.toString(message.getId())).build();
+		return uri.toString();
+ 	}
 
 	@POST
 	public Response addMessage(Message message, @Context UriInfo uriInfo)  {		/* Returns generic response object which adds the specific status code (201-CREATED) */
