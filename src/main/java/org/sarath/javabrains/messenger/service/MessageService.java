ea4c54debfab09ffca9b2d6ad6b50ad799d5ee56
Sarath S Nair <sarath.nair@asda.co.uk>
2016-05-31 18:14:29 +0530

    Adding exception if there is no message found in the server.

--- src/main/java/org/sarath/javabrains/messenger/service/MessageService.java
+++ src/main/java/org/sarath/javabrains/messenger/service/MessageService.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.sarath.javabrains.messenger.database.DatabaseClass;
+import org.sarath.javabrains.messenger.exception.DataNotFoundException;
 import org.sarath.javabrains.messenger.model.Message;
 
 /* MessageService - All the services to the MessageResource
@@ -46,7 +47,11 @@ public class MessageService {
 	}
 
 	public Message getMessage(long id) {
-		return messages.get(id);
+		Message message = messages.get(id);
+		if(message==null){
+			throw new DataNotFoundException("Message id "+id+" is not found");
+		}
+		return message;
 	}
 
 	public Message addMessage(Message message) {
